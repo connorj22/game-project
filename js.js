fe6d3337d3e0ec02c1c64b555faa1ac4087b279d
Connor Harris <connorharris@Connors-MBP-2.lan>
2022-10-06 14:32:53 -0400

    added end screen but buggy

--- js.js
+++ js.js
@@ -1,17 +1,19 @@
 startTitle.addEventListener("click", () => start());
-console.log("initiated")
 let clicks = 26;
 const body = document.body;
 let planetCount = 0;
 let scoreClick = 0;
 let startingPlanet = 5;
-let i = 0
 let menu = document.querySelector(".levelContainer")
-
 menu.style.display = "none"
+let endScreen = document.querySelector(".endScreen")
+endScreen.style.display = "none"
+let stats = document.querySelector(".stats")
+let winner = document.querySelector(".winner")
+let loser = document.querySelector(".loser")
+let planets = document.querySelectorAll(".planet")
 
 function start() {
-  console.log("start function called")
   let startTitle = document.querySelector("#startTitle");
   startTitle.remove();
   menu.style.display = "grid"
@@ -55,7 +57,6 @@ function impossibleMode() {
 
 
 function gameBrain() {
-  console.log(startingPlanet)
   scoreClick = 0;
   planetCount = 0
 
@@ -82,7 +83,6 @@ function gameBrain() {
 
   for (let i = 0; i < startingPlanet; i++) {
     createPlanet();
-    console.log(`created planet ${i}`)
   }
   game()
 }
@@ -91,18 +91,17 @@ function gameBrain() {
     function checkForWinner() {
       let planets = document.querySelectorAll(".planet");
       if (planets.length < 2) {
-        alert(`You Won! 
-      you used ${(scoreClick - 1) / planetCount} shots per target! 
-      you missed ${scoreClick - 1 - startingPlanet} Shots!
-      you had a shooting percentage of ${(startingPlanet / (scoreClick - 1)) * 100
-          }`);
-        console.log("winner")
-        i++
-        gameBrain()
+        endScreen.style.display = "grid"
+        stats.innerHTML = `You Won! 
+        you used ${(scoreClick - 1) / planetCount} shots per target! 
+        you missed ${scoreClick - 1 - startingPlanet} Shots!
+        you had a shooting percentage of ${(startingPlanet / (scoreClick - 1)) * 100
+        }`
+        winner.style.display = "grid" 
+        loser.style.display = "none"
       }
     }
 
-    console.log("game function is being called")
     let planets = document.querySelectorAll(".planet");
     let count = planets.length;
     planets.forEach((planet) => {
@@ -124,18 +123,22 @@ function gameBrain() {
       document.getElementById("counter").innerHTML = ` Ammo: ${clicks}`;
       endGame();
       checkForWinner();
-      console.log(`clicks: ${clicks}`, scoreClick)
     }
 
     function endGame() {
       if (clicks < 1) {
-        alert("You Suck!! Game Over, you ran out of Ammo");
-        window.location.reload();
+        endScreen.style.display = "grid"
+        loser.style.display = "grid"
+        winner.style.display = "none"
+        planets.style.display = "none"
       }
     }
   }
 
+  let reset = document.querySelector(".reset")
+  reset.addEventListener("click", () => window.location.reload())
 
-//make it so theirs four dificulties that we you can select from to start
-//code css popup at the end of each round and one when you loose that says the level you were on
+
+//fix bug where plants wont disapear when you lose and make the stats centered
+//change font to be pixelated
 //add simple instructions for the game on the start screen
